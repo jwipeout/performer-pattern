5c68efb98655aa4f4e9cb1ec593054f060655e5f
Joshua Cavin <jcavin9@gmail.com>
2017-07-21 17:24:12 -0700

    Use method missing to load helpers

--- app/support/helper.rb
+++ app/support/helper.rb
@@ -1,27 +1,28 @@
 class Helper
-  extend ActionView::Helpers::UrlHelper
-  extend ActionView::Helpers::TagHelper
-
-  class << self
-    def render(**args)
-      ApplicationController.render(args)
-    end
-
-    def routes
-      Rails.application.routes.url_helpers
-    end
+  Dir.glob("#{Rails.root}/app/helpers/*.rb") do |helper_file|
+    extend helper_file.split('/').last.split('.').first.classify.constantize
   end
 
-  class LoadCustomHelpers
-    class << self
-      def load_custom_helpers
-        Dir.glob("#{Rails.root}/app/helpers/*.rb") do |helper_file|
-          custom_helper = helper_file.split('/').last.split('.').first.classify.constantize
-          Helper.extend custom_helper
-        end
+  @routes = Rails.application.routes.url_helpers
+  @action_view = ActionView::Base.new('app/views')
+
+  class << self
+    attr_reader :routes, :action_view
+
+    def method_missing(method, *args, &block)
+      if routes.respond_to?(method)
+        routes.send(method, *args, &block)
+      elsif action_view.respond_to?(method)
+        action_view.send(method, *args, &block)
+      else
+        super
       end
     end
 
-    load_custom_helpers
+    def respond_to_missing?(method, include_private = false)
+      routes.respond_to?(method, include_private) ||
+        action_view.respond_to?(method, include_private) ||
+        super
+    end
   end
 end
